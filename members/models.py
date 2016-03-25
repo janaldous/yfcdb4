f699d03072711f25197baebac720bd7d31eb04aa
janaldous <jat.torres@gmail.com>
2016-03-25 14:50:35 +0000

    renamed gitignore, added members to settings,

--- members/models.py
+++ members/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 class Member(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
