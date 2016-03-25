b413300d80628233f3fc07506f82dce2dae579d7
janaldous <jat.torres@gmail.com>
2016-03-25 15:05:47 +0000

    registered member to admin, changed attributes for names in Member

--- members/models.py
+++ members/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Member(models.Model):
-    first_name = models.CharField(max_length=100)
+    family_name = models.CharField(max_length=100)
+    given_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return ("%s, %s '%s' %s" % (self.family_name, self.given_name, self.nickname, self.middle_name))
