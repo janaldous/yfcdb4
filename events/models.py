8bef7cba60712c86fb412d3688983dda6b8b8c75
janaldous <jat.torres@gmail.com>
2016-03-26 11:02:31 +0000

    customized EventAdmin, added str to Event model

--- events/models.py
+++ events/models.py
@@ -11,3 +11,6 @@ class Event(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=2, choices=EVENT_TYPE)
     venue = models.CharField(max_length=50)
+
+    def __str__(self):
+        return ("%s: %s at %s" % (self.date, self.name, self.venue))
