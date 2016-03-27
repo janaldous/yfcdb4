85881da11b15c79e98fd493f531dbe14223166bb
janaldous <jat.torres@gmail.com>
2016-03-27 12:32:38 +0100

    added bootstrap to static

--- yfcdb4/settings.py
+++ yfcdb4/settings.py
@@ -127,14 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/staticfiles/'
 
-# Extra places for collectstatic to find static files.
+# Extra places for collectstatic to find staticfiles files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
-# Simplified static file serving.
+# Simplified staticfiles file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
